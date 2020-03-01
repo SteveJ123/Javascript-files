8a68bbba0381240b7536183c29bd49d264faa749
unknown <jjanardhan@gmail.com>
2020-03-01 23:14:01 +0530

    added

---
+++ rune/test.js
@@ -0,0 +1,53 @@
+{
+    "id": 1430,
+    "post": 1712,
+    "parent": 1422,
+    "author": 43,
+    "author_name": "Mike Alex",
+    "author_url": "",
+    "date": "2020-02-27T06:47:15",
+    "date_gmt": "2020-02-27T06:47:15",
+    "content": {
+        "rendered": "<p>Try something New</p>\n"
+    },
+    "link": "https://www.localhost.com/news/unknowing/17312/#comment-14730",
+    "status": "approved",
+    "type": "comment",
+    "author_avatar_urls": {
+        "24": "https://www.localhost.com/wp-content/uploads/2020/02/cropped-26841328_586072901737986_8512739411932356338_o-small.jpg",
+        "48": "https://www.localhost.com/wp-content/uploads/2020/02/cropped-26841328_586072901737986_8512739411932356338_o-small.jpg",
+        "96": "https://www.localhost.com/wp-content/uploads/2020/02/cropped-26841328_586072901737986_8512739411932356338_o-small.jpg"
+    },
+    "meta": [],
+    "_links": {
+        "self": [
+            {
+                "href": "https://www.localhost.com/wp-json/wp/v2/comments/14730"
+            }
+        ],
+        "collection": [
+            {
+                "href": "https://www.localhost.com/wp-json/wp/v2/comments"
+            }
+        ],
+        "author": [
+            {
+                "embeddable": true,
+                "href": "https://www.localhost.com/wp-json/wp/v2/users/43"
+            }
+        ],
+        "up": [
+            {
+                "embeddable": true,
+                "post_type": "post",
+                "href": "https://www.localhost.com/wp-json/wp/v2/posts/17312"
+            }
+        ],
+        "in-reply-to": [
+            {
+                "embeddable": true,
+                "href": "https://www.localhost.com/wp-json/wp/v2/comments/14622"
+            }
+        ]
+    }
+},
